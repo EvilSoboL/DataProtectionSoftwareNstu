6f95b9a84e0fc2251bbdac5c479eb77a00b1a9e9
EvilSoboL <evilsobol01@gmail.com>
2024-09-10 20:10:03 +0700

    Рабочее чтение и редактирование сообщения, чтение ключа и зашифрованного сообщения

--- lab1/view_message_key.py
+++ lab1/view_message_key.py
@@ -21,10 +21,29 @@ class ViewMessageKeyWindow(QWidget):
         file_path, _ = file_dialog.getOpenFileName(self, "Выбор файла", "", "Text Files (*.txt)")
 
         if file_path:
-            with open(file_path, 'r', encoding='utf-8') as file:
-                content = file.read()
-                bin_content = ' '.join(format(ord(c), '08b') for c in content)
-                hex_content = binascii.hexlify(content.encode()).decode('utf-8')
+            # Проверка на ключ или зашифрованный файл
+            if file_path.endswith('_key.txt') or file_path.endswith('_crypted.txt'):
+                self.read_binary_file(file_path)
+            else:
+                self.read_text_file(file_path)
 
-                self.label.setText(
-                    f"Символьный: {content}\n\nДвоичный: {bin_content}\n\nШестнадцатеричный: {hex_content}")
+    def read_text_file(self, file_path):
+        """Чтение и отображение текстового файла."""
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+            bin_content = ' '.join(format(ord(c), '08b') for c in content)
+            hex_content = binascii.hexlify(content.encode()).decode('utf-8')
+
+            self.label.setText(f"Символьный: {content}\n\nДвоичный: {bin_content}\n\nШестнадцатеричный: {hex_content}")
+
+    def read_binary_file(self, file_path):
+        """Чтение и отображение бинарного файла (ключа или зашифрованного сообщения)."""
+        with open(file_path, 'rb') as file:
+            content = file.read()
+            bin_content = ' '.join(format(byte, '08b') for byte in content)
+            hex_content = binascii.hexlify(content).decode('utf-8')
+
+            # Символьное представление для бинарного файла не показываем
+            char_content = "Символьное представление недоступно для бинарного файла."
+
+            self.label.setText(f"{char_content}\n\nДвоичный: {bin_content}\n\nШестнадцатеричный: {hex_content}")
